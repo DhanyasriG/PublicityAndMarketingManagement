198a6c2edf73cb583e76797a61e27487a20076c4
dhanyasrigrandhi <2100030130@kluniversity.in>
2024-06-26 15:31:58 +0530

    final commit

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -4,13 +4,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('login',views.login,name="login"),
     path('', views.index, name="index"),
     path('registration',views.registration,name="registration"),
     path('newspaper',views.newspaper,name="newspaper"),
     path('pomplet',views.pomplet,name="pomplet"),
     path('writer',views.writer,name="writer"),
-    path('index',views.index,name="index"),
+   # path('index',views.index,name="index"),
     path('checklogin',views.checklogin,name="checklogin"),
     path('horror',views.horror,name="horror"),
     path('action',views.action,name="action"),
